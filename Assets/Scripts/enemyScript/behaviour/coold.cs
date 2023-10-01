5f198b4a11046a653f1c423027aa91eb5a11429c
Maritsa Ariyani <chimmiyeay123@gmail.com>
2023-10-01 23:43:50 +0700

    Added

--- Assets/Scripts/enemyScript/behaviour/coold.cs
+++ Assets/Scripts/enemyScript/behaviour/coold.cs
@@ -1,29 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class attack : StateMachineBehaviour
-{ 
+public class coold : StateMachineBehaviour
+{
     float timer;
-    Transform player;
+    public int timeCooldown;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
         timer=0;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.transform.LookAt(player);
-        float distance = Vector3.Distance(player.position, animator.transform.position);
-        if (distance > 2f)
-            animator.SetBool("isAttacking",false);
         timer+=Time.deltaTime;
-        if (timer>1)
-            {animator.SetBool("isKick",true);}
+        if (timer > timeCooldown)
+            animator.SetBool("isWait", false);
     }
 }
