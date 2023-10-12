7a54dff8dc1e03f0dcf0c5a4cc792f5b04274b7f
Keen[] <77962920+Keen-taro@users.noreply.github.com>
2023-10-13 06:26:25 +0700

    FreshPush
    ntahlah gk ngerti banyak conflict

---
+++ Assets/Scripts/Canvas/goScripts.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goScripts : MonoBehaviour
+{
+    public GameObject gameOverUI;
+
+    private void Start()
+    {
+        gameOverUI.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("StageSelection");
+    }
+}
