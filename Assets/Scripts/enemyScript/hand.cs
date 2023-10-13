5341634fed7abf9c189fd9f920aa60d01904707c
Daffa Naufal Santoso <68911129+daffanaufal@users.noreply.github.com>
2023-10-13 20:33:36 +0700

    Revert "Adding Health and Game Over"

--- Assets/Scripts/enemyScript/hand.cs
+++ Assets/Scripts/enemyScript/hand.cs
@@ -4,12 +4,5 @@ using UnityEngine;
 
 public class hand : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player") //&& enemy status == "attack"
-        {
-            other.GetComponent<Player_Health>().TakeDamage(0.5f);
-            Debug.Log("Hit");
-        }
-    }
+    
 }
