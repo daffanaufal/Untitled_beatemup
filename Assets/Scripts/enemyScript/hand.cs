246135b68d7239d7dc6b29eb9e3f8bb24f61e103
Maritsa Ariyani <chimmiyeay123@gmail.com>
2023-10-14 13:57:28 +0700

    Added

--- Assets/Scripts/enemyScript/hand.cs
+++ Assets/Scripts/enemyScript/hand.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class hand : MonoBehaviour
 {
-    
+    public float damage;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") //&& enemy status == "attack"
+        {
+            other.GetComponent<Player_Health>().TakeDamage(damage);
+            //Debug.Log("Hit");
+        }
+
+    }
 }
